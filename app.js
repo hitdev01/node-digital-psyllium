45c57602be71268436208e3ed60aee3393ad8266
hitdev <hitdev01@gmail.com>
2016-09-05 18:06:36 +0900

    digital psyllium

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
+var connects = [];
+var colors = new Array('#000000','#FFFFFF','#F0F000','#FF0000','#00FF00','#0000FF');
+var color = '#FFFFFF';
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app)
-server.listen(port)
+var server = http.createServer(app);
+server.listen(port);
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+console.log("websocket server created");
 
-wss.on("connection", function(ws) {
+
+
+wss.on('connection', function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    ws.send(color, function() {  });
+    console.log('send messag['+ color +']');
+  }, 1000);
 
-  console.log("websocket connection open")
+  console.log('websocket connection open');
+  // 接続してきたソケットを格納
+  connects.push(ws);
+  console.log('connected sockets: ' + connects.length);
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
+
+  ws.on('message', function(message) {
+    console.log('get message['+ message +']');
+    broadcast(message);
+  })
+
+  ws.on('close', function() {
+    console.log('websocket connection close');
+    // 接続切れのソケットを配列から除外
+    connects = connects.filter(function (conn, i) {
+        return (conn === ws) ? false : true;
+    });
+    console.log('connected sockets: ' + connects.length);
+    clearInterval(id);
   })
 })
+
+setInterval(function chengecolor() {
+  var index = Math.floor(Math.random() * (colors.length));
+  color = colors[index];
+}, 10000);
